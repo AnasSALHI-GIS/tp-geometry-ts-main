3be7e167f7ae01188f364e794c725d054b7e31dc
@Anas_Salhi <anas.salhi@ensg.eu>
2025-11-13 09:55:38 +0100

    1ère question TP

---
+++ src/LineString.ts
@@ -0,0 +1,22 @@
+import Coordinate from "./Coordinate";
+import Geometry from "./Geometry";
+import Point from "./Point";
+
+
+
+export default class LineString {
+  private points: Point[];  
+  constructor (points?: Point[]){
+    this.points=points;
+  }
+  getType():string{
+    return("LineString");
+  }
+    getNumPoints(): number{
+    return this.points? this.points.length:0;
+    }
+    getPointN(n:number): Point{
+        return this.points[n];
+
+    }   
+}
---
+++ src/Point.ts
@@ -0,0 +1,28 @@
+import Coordinate from "./Coordinate";
+import Geometry from "./Geometry";
+
+
+export default class Point implements Geometry {
+  private coordinate?: Coordinate;
+
+  constructor(coordinate?: Coordinate) {
+    this.coordinate = coordinate ;
+  }
+
+  getType(): string {
+    return("Point")
+  }
+
+  getCoordinate(): Coordinate {
+    return this.coordinate;
+  }
+
+  x(): number {
+    return this.coordinate ? this.coordinate[0] : Number.NaN ;
+  }
+
+  y(): number {
+    return this.coordinate ? this.coordinate[1] : Number.NaN ;
+  }
+
+}
