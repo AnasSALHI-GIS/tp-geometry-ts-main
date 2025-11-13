40e940182613673ba8790462d321332723a2862b
@Anas_Salhi <anas.salhi@ensg.eu>
2025-11-13 17:06:51 +0100

    QUestion 5 suite

--- src/EnvelopeBuilder.ts
+++ src/EnvelopeBuilder.ts
@@ -1,7 +1,15 @@
 import Coordinate from "./Coordinate";
 import Envelope from "./Envelope";
 
-export default interface EnvelopeBuilder{
-    insert(Coordinate:Coordinate);
-    build():Envelope;
+export default class EnvelopeBuilder{ 
+     Xvals:number[];
+     Yvals:number[];
+    insert(coordinate:Coordinate){
+        this.Xvals.push(coordinate[0]);
+    }
+    build():Envelope{
+        const Xmin=Math.min(...this.Xvals);
+
+        return;
+    }
 }
